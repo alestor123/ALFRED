76f1ccf55cac5974214291e1283c10a4f3fda126
Alestor Aldous <alestoraldous@gmail.com>
2024-08-02 11:08:32 +0530

    chore: Added scripts to package.json

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 const telegramBot = require('node-telegram-bot-api');
 const responses = require('./controllers/responses.js');
 const prompt = require('./controllers/prompt.js');
+const leetcode = require('./controllers/leetcode.js');
+
 
 
 
@@ -9,4 +11,6 @@ const bot = new telegramBot(token, { polling: true });
 // bot.on('message',(msg) => )
 bot.onText(/^!.*$/, (msg) => responses(msg,bot));
 bot.onText("/register", (msg) => prompt(msg,bot));
+bot.onText("!leetcoe", (msg) => leetcode(msg,bot));
+
 }
---
+++ controllers/leetcode.js
@@ -0,0 +1,10 @@
+module.exports = (msg,bot) => {
+    
+    const chatId = msg.chat.id;
+    const messageText = msg.text;
+    console.log(messageText)
+    // if (messageText=="/register") ;
+// console.log(chatId)
+    // if (responseData[messageText]) bot.sendMessage(chatId, responseData[messageText]);
+    // else bot.sendMessage(chatId, responseData["404"]);
+}
